5292fe8584fdcb616ced06d99d4134a736fd066c
TransDiv <transdiv@gmail.com>
2026-05-21 12:25:35 -0500

    Flip player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
- using UnityEngine;
+using System;
+using UnityEditor.Tilemaps;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private GatherInput m_gatherInput;
     private Transform m_transform;
     [SerializeField] private float speed;
+    private int direction = 1;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +21,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Move();
+    }
+
+    private void Move()
+    {
+        Flip();
         m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, m_rigidbody2D.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        if (m_gatherInput.ValueX * direction < 0)
+        {
+            direction *= -1;
+            m_transform.localScale = new Vector3(-m_transform.localScale.x, 1, 1);
+        }
+
     }
 }
